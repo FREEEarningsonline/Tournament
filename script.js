132f0964cad2724e7de4c36bf153d6327092a9b2
Ghulam Mustafa <nm2294038@gmail.com>
2026-03-25 13:11:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1686,7 +1686,7 @@ async function claimDailyBonus() {
         }
 
         // Generate random bonus between 10 and 1000 (inclusive)
-        const randomBonus = Math.floor(Math.random() * 991) + 10;
+        const randomBonus = Math.floor(Math.random() * 80) + 10;
         
         let committed = false;
         await userRef.transaction(data => {
